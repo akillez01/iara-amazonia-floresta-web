3c8cb05671d82bb7ca09c055b5cce90f74371023
Akilles01 <achilles.oliveira.souza@gmail.com>
2025-05-28 12:32:16 -0400

    Ajustes para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/iara-amazonia-floresta-web/',
+  base: '/iara-amazonia-floresta-web/',
   server: {
     host: "::",
     port: 8080,
